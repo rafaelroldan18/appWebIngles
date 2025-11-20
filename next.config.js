2b632a42b9537dc2ab295a35ad56fbd4ed543a60
Rafael Roldan <137737500+rafaelroldan18@users.noreply.github.com>
2025-11-20 16:12:25 -0500

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   images: {
     domains: [],
   },
+}
 
-  // 🔧 Desactivar Turbopack en Next.js 16
-  experimental: {
-    turbo: {
-      enabled: false,
-    },
-  },
-
-  // 🔧 Asegurar que Webpack se use como compilador
-  webpack(config) {
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
